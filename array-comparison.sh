ff83f5a890d3eb423862771e663ae91744102dbf
keanu-reevz <bastien.computer@gmail.com>
2020-02-14 11:42:49 +0100

    Adding array-comparison script

---
+++ array-comparison.sh
@@ -0,0 +1,21 @@
+a=( 3 5 8 10 6 ) 
+b=( 6 5 4 12 )
+c=( 14 7 5 7 )
+
+for i in "${a[@]}"; do
+	for j in "${b[@]}"; do
+		if [ $i = $j ]; then 
+			d[${d[@]}]=$i
+		fi
+	done
+done
+
+for k in "${c[@]}"; do
+	for l in "${d[@]}"; do
+		if [ $k = $l ]; then
+			e[${e[@]}]=$k
+		fi
+	done
+done
+
+echo ${e[@]}
